e58fa38713f64672fd44df391c8fe41f121a6795
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-05-20 22:45:37 -0300

    feat: Made general improvements

--- collection-archive-sql/08_fx_cursos/fx-cursos.sql
+++ collection-archive-sql/08_fx_cursos/fx-cursos.sql
@@ -190,3 +190,21 @@ ON a.idAluno = m.idAluno
 INNER JOIN curso c
 ON c.idCurso = m.idCurso
 WHERE m.dataMatricula BETWEEN '2025-03-01' AND '2025-03-31';
+
+-- |===========|
+-- |Exercício 9| 
+-- |===========|
+# Quais alunos não tem matrícula ativa
+SELECT a.nomeAluno
+FROM aluno a
+LEFT JOIN matricula m ON a.idAluno = m.idAluno
+WHERE m.dataMatricula IS NULL;
+
+-- |===========|
+-- |Exercício 10| 
+-- |===========|
+#Quais os cursos que estão sem matriculas
+SELECT c.nomeCurso
+FROM curso c
+LEFT JOIN matricula m ON c.idCurso = m.idCurso
+WHERE m.dataMatricula IS NULL;
