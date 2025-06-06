fb25736cdd46648ce85055efbc49e65d4cd6ff6c
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-06-06 01:00:51 -0300

    feat: Added comments and "DCL" commands

--- collection-archive-sql/13_empresa_woop/empresa-woop.sql
+++ collection-archive-sql/13_empresa_woop/empresa-woop.sql
@@ -1,20 +1,49 @@
 CREATE DATABASE empresa_woop;
 
-CREATE ROLE desenvolvimento, gerente, venda;
+-- Cria os papéis (roles)
+CREATE ROLE IF NOT EXISTS 'desenvolvimento', 'gerente', 'venda';
 
-SELECT User, Host FROM mysql.user;
+SELECT user, host FROM mysql.user 
+WHERE user IN ('desenvolvimento', 'gerente', 'venda');
 
 CREATE USER 'diego'@'localhost' IDENTIFIED BY 's3nh@';
 CREATE USER 'marcia'@'localhost' IDENTIFIED BY 'abc1234';
 CREATE USER 'joana'@'localhost' IDENTIFIED BY 'user123';
 
+SELECT user, host FROM mysql.user 
+WHERE user IN ('diego', 'marcia', 'joana');
+
+-- Associa os papéis aos usuários correspondentes
 GRANT desenvolvimento TO diego@localhost;
 GRANT gerente TO marcia@localhost;
 GRANT venda TO joana@localhost;
 
-SHOW GRANTS for 'desenvolvimento';
+-- Consulta as relações entre usuários e papéis
+SELECT from_user AS usuario, to_user AS role 
+FROM mysql.role_edges;
 
--- Mostrar todos que fazem parte de cada grupo
-SHOW TABLES FROM MYSQL LIKE 'roles_mapping';
+-- Mostra as permissões dos papéis
+SHOW GRANTS FOR 'desenvolvimento';
+SHOW GRANTS FOR 'gerente';
+SHOW GRANTS FOR 'venda';
 
-GRANT ALL PRIVILEGES ON *.* TO desenvolvimento;
+
+-- Lista todas as relações entre papéis e usuários
+SELECT * FROM mysql.role_edges;
+
+-- Lista papéis e seus usuários ordenados pelo nome do papel
+SELECT to_user AS role_name, from_user AS user_name 
+FROM mysql.role_edges 
+ORDER BY to_user;
+
+GRANT ALL PRIVILEGES ON *.* TO 'desenvolvimento';
+GRANT SELECT, INSERT, UPDATE, DELETE ON empresa_woop.* TO 'gerente';
+GRANT SELECT ON empresa_woop.* TO 'venda';
+
+GRANT ALL PRIVILEGES ON *.* TO 'desenvolvimento';
+GRANT SELECT ON empresa_woop.* TO 'gerente';
+GRANT SELECT ON empresa_woop.* TO 'venda';
+
+DROP USER 'marcia'@'localhost';
+
+DROP ROLE 'gerente';
